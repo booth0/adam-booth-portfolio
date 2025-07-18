0edbde0050924b95e0c9cdf8e90d5841d792a8e6
booth0 <10toasterv2@gmail.com>
2025-07-18 10:55:38 -0600

    server js started

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 import express from 'express';
 
 const app = express();
+
+app.length('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is runnin on http://127.0.0.1:${PORT}`);
+});
